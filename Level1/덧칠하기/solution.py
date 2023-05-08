c9d53beebe40d4b20176e61487d28608d0d77b32
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-05-09 02:18:54 +0900

    solution

--- Level1/덧칠하기/solution.py
+++ Level1/덧칠하기/solution.py
@@ -4,9 +4,7 @@ def solution(n, m, section):
     while section[-1] - section[i] + 1 > m:
         answer += 1
         j = i
-        k = i
-        while section[j+1] - section[k] + 1 <= m:
+        while section[i+1] - section[j] + 1 <= m:
             i += 1
-            j += 1
         i += 1
     return answer
