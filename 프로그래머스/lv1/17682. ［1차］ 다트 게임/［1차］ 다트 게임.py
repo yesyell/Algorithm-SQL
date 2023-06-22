848e9ccc47bb1aed88473669afae284c9844df47
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-06-22 14:28:31 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.17 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,9 +1,10 @@
 import re
 
 def solution(dartResult):
-    answer = []
-    for i, j, k in re.findall(r'(10|[0-9])([SDT])([*#]?)', dartResult):
-        print(i, j, k)
+    result = []
+    pattern = re.compile(r'(10|[0-9])([SDT])([*#]?)')
+    
+    for i, j, k in pattern.findall(dartResult):
         if j == 'S':
             n = int(i)
         elif j == 'D':
@@ -12,9 +13,10 @@ def solution(dartResult):
             n = int(i)**3
         if k == '*':
             n *= 2
-            if len(answer) > 0:
-                answer[-1] *= 2
+            if len(result) > 0:
+                result[-1] *= 2
         elif k == '#':
             n *= -1
-        answer.append(n)
-    return sum(answer)
+        result.append(n)
+        
+    return sum(result)
