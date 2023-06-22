c7697f138075ed47afbd8a11fabe298478002c8b
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-06-22 15:37:06 +0900

    Delete Level1 directory

--- Level1/과일장수/solution.py
+++
@@ -1,8 +0,0 @@
-def solution(k, m, score):
-    answer = 0
-    score_s = sorted(score, reverse=True)
-    for i in range(0, len(score), m):
-        if len(score) >= i+m:
-            k = min(score_s[i:i+m])
-            answer += k * m
-    return answer
