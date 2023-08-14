0bd89cd36802f02c82e80a63a96d1db79889be70
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-08-14 23:16:03 +0900

    [level 1] Title: 최소직사각형, Time: 3.58 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,8 +1,9 @@
 def solution(sizes):
-    max_s = 0
-    min_l = []
-    for size in sizes:
-        if max(size) > max_s:
-            max_s = max(size)
-        min_l.append(min(size))
-    return max_s * max(min_l)
+    # max_s = 0
+    # min_l = []
+    # for size in sizes:
+    #     if max(size) > max_s:
+    #         max_s = max(size)
+    #     min_l.append(min(size))
+    # return max_s * max(min_l)
+    return max(max(x) for x in sizes) * max(min(x) for x in sizes)
