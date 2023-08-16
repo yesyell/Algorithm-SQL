06c00093e15ad96cd9ed46a75098dc1e1121e811
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-08-16 23:32:31 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.44 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,13 +1,19 @@
 def solution(left, right):
-    s = 0
-    ll = []
+    answer = 0
     for i in range(left, right+1):
         l = []
         for j in range(1, i+1):
             if i % j == 0:
                 l.append(j)
-        s = i
-        if len(l) % 2 != 0:
-            s *= -1
-        ll.append(s)
-    return sum(ll)
+        if len(l) % 2 == 0:
+            answer += i
+        else:
+            answer -= i
+    return answer
+
+    # for i in range(left, right+1):
+    #     if int(i**0.5)==i**0.5: # 제곱수이면
+    #         answer -= i
+    #     else:
+    #         answer += i
+    # return answer
