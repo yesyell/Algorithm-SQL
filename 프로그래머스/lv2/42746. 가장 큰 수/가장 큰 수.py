d7bb67ef8c0739b5423a02f28e40d3be721514be
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-09-30 23:57:36 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,17 +1,4 @@
-# from itertools import permutations
-# def solution(numbers):
-#     m = 0
-#     for i in list(permutations(numbers, len(numbers))):
-#         s = ''
-#         for j in range(len(numbers)):
-#             s += str(i[j])
-#             m = max(int(s), m)
-#     return str(m)
-
 def solution(numbers):
-    if max(numbers) == 0:
-        return "0"
-    numbers = list(map(lambda x:str(x)*3,numbers))
-    numbers.sort(reverse=True)
-
-    return ''.join(map(lambda x:x[:len(x)//3],numbers))
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers)))
