2ba5a892320b08fa09151133b0e90d8134a36418
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-10-02 23:26:09 +0900

    [level 2] Title: 올바른 괄호, Time: 6.58 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,22 @@
+# def solution(s):
+#     a = 0
+#     for k in s:
+#         if k =='(':
+#             a += 1
+#         else:
+#             a -= 1
+#         if a == -1:
+#             return False
+#     return a == 0
+
 def solution(s):
-    a = 0
-    for k in s:
-        if k =='(':
-            a += 1
+    st = []
+    for c in s:
+        if c == '(':
+            st.append(c)
         else:
-            a -= 1
-        if a == -1:
-            return False
-    if a > 0:
-        return False
-    return True
+            try:
+                st.pop()
+            except IndexError:
+                return False
+    return len(st) == 0
